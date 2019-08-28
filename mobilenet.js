f92b9d400df35d43b0306552642f6117c71b51f3
ashlinaronin <ashlinaronin@gmail.com>
2019-08-27 22:20:37 -0500

    use fish and nofish for classes

--- mobilenet.js
+++ mobilenet.js
@@ -40,10 +40,8 @@ async function app() {
   };
 
   // When clicking a button, add an example for that class.
-  document.getElementById('class-a').addEventListener('click', () => addExample(0));
-  document.getElementById('class-b').addEventListener('click', () => addExample(1));
-  document.getElementById('class-c').addEventListener('click', () => addExample(2));
-
+  document.getElementById('class-fish').addEventListener('click', () => addExample(0));
+  document.getElementById('class-nofish').addEventListener('click', () => addExample(1));
 
   while (true) {
     if (classifier.getNumClasses() > 0) {
@@ -52,7 +50,7 @@ async function app() {
       // Get the most likely class and confidences from the classifier module.
       const result = await classifier.predictClass(activation);
 
-      const classes = ['A', 'B', 'C'];
+      const classes = ['fish', 'nofish'];
       document.getElementById('console').innerText = `
         prediction: ${classes[result.classIndex]}\n
         probability: ${result.confidences[result.classIndex]}
